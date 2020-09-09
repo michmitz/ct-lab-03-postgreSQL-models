c48e8439448ab9bd82e5160c5a2b2df40747f9ab
Michelle Stermitz <stermitzm@gmail.com>
2020-09-08 18:06:17 -0700

    Added post and delete endpoints with TDD

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,15 +2,41 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const Fruit = require('../lib/models/fruit');
 
-describe('ct-lab-03-postgreSQL-models routes', () => {
+describe('routes', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
 
-  describe('classes routes', () => {
-    it('adding dummy test to remove fail message', () => {
-      expect('yes').toEqual('yes');
+  it('create a new fruit via POST', async() => {
+    const response = await request(app)
+      .post('/api/v1/fruits')
+      .send({ name: 'strawberry', color: 'red', weight: 3 });
+
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      name: 'strawberry',
+      color: 'red',
+      weight: '3'
+    });
+  });
+
+  it('deletes a fruit by id via DELETE', async() => {
+    const createdFruit = await Fruit.insert({
+      name: 'strawberry',
+      color: 'red',
+      weight: 3
+    });
+
+    const response = await request(app)
+      .delete(`/api/v1/fruits/${createdFruit.id}`);
+
+    expect(response.body).toEqual({
+      id: createdFruit.id,
+      name: 'strawberry',
+      color: 'red',
+      weight: '3'
     });
   });
 });
